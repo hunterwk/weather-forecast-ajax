100f716e721f188abe7cea83f785ee734d03efb1
Hunter Kantner <hunterkantner@gmail.com>
2020-09-21 22:45:27 -0700

    added notation and began to construct the UV index api call

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //Opening call function
 $(function () {
-    /* Day js isn't working for some reason.
-    currentDay = " "
-    currentDay.textContent = dayjs().format();
-    console.log(currentDay)
-    */
+    // Checks local storage for stored city variables and if it exists
+    // runs the code to display the information from previous search
+    init();
+    function init() {
+
+    };
+    
+    //CITY VAR WILL BE ENTERED BY USER
     var city = "San Diego";
-    var apiKey = "ae72ba1c8b5248666070d873a02673f9";
+    var apiKey = "YOUR_API_KEY";
+    //initial function to call the temp,humidity and wind speed
     function cityWeather() {
         var queryUrl =
             "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -23,8 +27,12 @@ $(function () {
             $("#selected-temperature").text(data.main.temp);
             $("#selected-humidity").text(data.main.humidity);
             $("#selected-wind-speed").text(data.wind.speed);
-            
-            
+
+            //using the called latitude and longitude
+            //determine the UV index for user specified city
+            var latitude = data.coord.lat
+            var longitude = data.coord.lon
+
         })
 
     }
